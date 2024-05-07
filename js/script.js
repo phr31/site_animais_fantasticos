87341e77a682c1840a67ce2f389339dc615aa081
phr31 <phr31@hotmail.com>
2024-05-07 15:33:01 -0300

    Adicionado Tooltip ao mapa na sessão contato

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from './modules/animacao-scroll.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabNav.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
 
 initTabNav();
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initModal();
+initTooltip();
