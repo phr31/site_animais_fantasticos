b14ae32a1e6dea40b2f99563341e45a726f7a110
phr31 <phr31@hotmail.com>
2024-05-17 19:53:03 -0300

    menu mobile

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import initTabNav from './modules/tabNav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
 
 initTabNav();
 initScrollSuave();
@@ -13,3 +14,4 @@ initAccordion();
 initModal();
 initTooltip();
 initDropdownMenu();
+initMenuMobile();
