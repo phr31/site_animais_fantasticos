b955025849d663e7dbcbb6fe01e21c32050260a4
phr31 <phr31@hotmail.com>
2024-06-13 20:51:58 -0300

    Criação Debounce

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -1,8 +1,10 @@
+import debounce from "./debounce.js";
+
 export default class AnimacaoScroll {
   constructor(target) {
     this.sections = document.querySelectorAll(target);
     this.windowMetade = window.innerHeight * 0.6;
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 100);
   }
 // Pega a distancia de cada item em relação ao topo do site
   getDistance() {
