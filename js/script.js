9ef27ffc07a7261397620c35628443171ed4e414
phr31 <phr31@hotmail.com>
2024-06-13 23:22:51 -0300

    Refatorar Funcionamento

--- js/script.js
+++ js/script.js
@@ -7,10 +7,9 @@ import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
 import AnimacaoScroll from "./modules/animacao-scroll.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
-
 import MenuMobile from "./modules/menu-mobile.js";
 
-import initFuncionamento from "./modules/funcionamento.js";
+import Funcionamento from "./modules/funcionamento.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -45,5 +44,7 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
-initFuncionamento();
+const funcionamento = new Funcionamento("[data-semana]");
+funcionamento.init();
+
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
